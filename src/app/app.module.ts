2c631fd5ab8807096f97edd99d5b12edf29b6c61
Mike Huang <wellwind@mail2000.com.tw>
2020-02-07 11:30:17 +0800

    add ngDoBootstrap (which doesnt fired)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,4 +13,10 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  ngDoBootstrap() {
+    console.log('test');
+  }
+
+}
